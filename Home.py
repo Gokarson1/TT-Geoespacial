03ca72b783b47dd663d3002e6b83ebc64f381a74
VlackVist <pvargasm@utem.cl>
2024-12-16 23:37:53 -0300

    hotfix2

--- Home.py
+++ Home.py
@@ -38,30 +38,30 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     st.markdown("**Area limitrofe de la región Metropolitana**")
-    st.image("img/home1.png")
+    st.image("img/home1.png",use_column_width=True)
 
 with col2:
     st.markdown("**Población segun ciudades en Estados Unidos**")
-    st.image("img/home2.png")
+    st.image("img/home2.png",use_column_width=True)
 
 with col3:
     st.markdown("**Infraestructura institucional en Magallanes**")
-    st.image("img/home3.png")
+    st.image("img/home3.png",use_column_width=True)
 
 # Segunda fila con 3 columnas adicionales
 col4, col5, col6 = st.columns(3)
 
 with col4:
     st.markdown("**Análisis de yacimientos mineros en el norte**")
-    st.image("img/home4.png", use_container_width=True)
+    st.image("img/home4.png",use_column_width=True)
 
 with col5:
     st.markdown("**Aeropuertos en territorio nacional**")
-    st.image("img/home5.png", use_container_width=True)
+    st.image("img/home5.png",use_column_width=True)
 
 with col6:
     st.markdown("**Limites urbanos de la provincia Chacabuco**")
-    st.image("img/home6.png", use_container_width=True)
+    st.image("img/home6.png", use_column_width=True)
 
 #crea el mapa
 #m = folium.Map(location=[-33.466, -70.597], zoom_start=10)
